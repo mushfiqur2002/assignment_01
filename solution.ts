775dcbae33111253a3aebe3368486e8b08c9e8ea
mushfiqur2002 <mushfiqurnasim@gmail.com>
2025-11-17 18:25:29 +0600

    ###

--- solution.ts
+++ solution.ts
@@ -1,4 +1,3 @@
-// problem 01 
 const formatValue = (value: string | number | boolean) => {
     if (typeof value === 'string') {
         return value.toUpperCase()
@@ -8,11 +7,8 @@ const formatValue = (value: string | number | boolean) => {
         return !value
     }
 }
-// console.log(formatValue('hello'));
-// console.log(formatValue(5));
-// console.log(formatValue(true));
-// ----------- //
-// problem 02
+
+
 const getLength = (value: string | string[] | number[]): number => {
     if (typeof value === 'string') {
         return value.length
@@ -21,10 +17,7 @@ const getLength = (value: string | string[] | number[]): number => {
     }
     return 0
 }
-// console.log(getLength('typescript'));
-// console.log(getLength(['yoolo', 'boo', 'tho', 'lo', 'do']));
-// ------------- //
-// problem 03 
+
 
 class Person {
     name: string;
@@ -37,14 +30,7 @@ class Person {
         return `Name: ${this.name}, Age: ${this.age}`
     }
 }
-// const person1 = new Person('John Doe', 30);
-// console.log(person1.getDetails());
 
-// const person2 = new Person('Alice', 25);
-// console.log(person2.getDetails());
-
-// ------------- //
-// problem 04
 
 type TBooksItems = {
     title: string;
@@ -53,17 +39,8 @@ type TBooksItems = {
 const filterByRating = (arr: TBooksItems[]) => {
     return arr.filter((item) => item.rating >= 4)
 }
-// const books: booksItems[] = [
-//     { title: 'Book A', rating: 4.5 },
-//     { title: 'Book B', rating: 3.2 },
-//     { title: 'Book B', rating: 4 },
-//     { title: 'Book C', rating: 5.0 },
-// ];
-// console.log(filterByRating(books));
 
 
-// ------------- //
-// problem 05
 type IUser = {
     id: number
     name: string
@@ -74,17 +51,7 @@ const filterActiveUsers = (userArr: IUser[]) => {
     return userArr.filter((user) => user.isActive === true)
 }
 
-// const users: IUser[] = [
-//     { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-//     { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-//     { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-// ];
 
-// console.log(filterActiveUsers(users));
-
-
-// ------------- //
-// problem 06
 interface Book {
     title: string
     author: string
@@ -93,52 +60,27 @@ interface Book {
 }
 const printBookDetails = (books: Book) => {
     return console.log(`Title: ${books.title}, Author: ${books.author}, Published: ${books.publishedYear}, Available: ${books.isAvailable ? 'Yes' : 'No'}`);
-
 }
 
-// const myBook: Book = {
-//     title: 'The Great Gatsby',
-//     author: 'F. Scott Fitzgerald',
-//     publishedYear: 1925,
-//     isAvailable: false,
-// };
-// printBookDetails(myBook);
 
-// ------------- //
-// problem 07
 const getUniqueValues = (arr1: number[], arr2: number[]) => {
     let arr = [...arr1, ...arr2]
     return Array.from(new Set(arr))
 }
 
-// const array1 = [1, 2, 3, 4, 5];
-// const array2 = [3, 4, 5, 6, 7];
-// console.log(getUniqueValues(array1, array2));
 
-// ------------- //
-// problem 08
 type IProduct = {
     name: string
     price: number
     quantity: number
     discount?: number
 }
-
 const calculateTotalPrice = (arr: IProduct[]) => {
     let totalPrice = 0
     arr.map((item) => {
-        // totalPrice += (item.price * item.quantity) * (item.discount / 100)
         let basePrice = item.price * item.quantity
         let finalPrice = item.discount ? basePrice - (basePrice * (item.discount / 100)) : basePrice
         totalPrice += finalPrice
     })
     return totalPrice
 }
-const products: IProduct[] = [
-        { name: 'Pen', price: 10, quantity: 2 },
-        { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
-        { name: 'Bag', price: 50, quantity: 1, discount: 20 },
-];
-
-console.log(calculateTotalPrice(products));
-// console.log(calculateTotalPrice(products));
